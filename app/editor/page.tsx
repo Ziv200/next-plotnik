c9a8143494a526bc484937a99a97254b0288c19c
Ziv200 <ziv200@gmail.com>
2024-01-19 00:37:47 +0200

    bug fixed

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,25 +1,15 @@
-"use client";
-
 import EditorAddObjectList from "./_components/EditorAddObjectList";
 import EditorInfoLine from "./_components/EditorInfoLine";
 import EditorMiniTable from "./_components/EditorMiniTable";
 import EditorObjectPropeties from "./_components/EditorObjectPropeties";
 import EditorStage from "./_components/EditorStage";
 
-import { usePlan } from "./planContext";
 const EditPage = () => {
-  const { editorSettings } = usePlan();
   return (
     <div className='flex flex-col lg:flex-row  justify-between p-5 gap-5'>
       <div id='editor_sidebar' className='flex flex-col justify-around items-center w-1/3 max-w-[400px] bg-gray-200'>
-        {editorSettings.sidebarMode === "edit" ? (
-          <>
-            <EditorMiniTable />
-            <EditorObjectPropeties />
-          </>
-        ) : (
-          <EditorAddObjectList />
-        )}
+        <EditorMiniTable />
+        <EditorObjectPropeties />
       </div>
       <div className='flex  flex-col gap-5 mx-auto'>
         <EditorInfoLine />
